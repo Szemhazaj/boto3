e754162a7793d976c623f37bc42d2e174899958b
SzemhazaJ <tenten223@gmail.com>
2025-06-03 20:07:57 +0200

    Add create_bucket_method. Use the method.

--- S3_with_boto_demo/main.py
+++ S3_with_boto_demo/main.py
@@ -6,9 +6,22 @@ import uuid
 
 
 # Method to create unique bucket name
-@log_calls()
+# @log_calls()
 def create_bucket_name(bucket_prefix):
     return ''.join([bucket_prefix, str(uuid.uuid4())])
+
+
+def create_bucket(bucket_prefix, s3_connection):
+    session_f = boto3.session.Session()
+    current_region = session_f.region_name
+    print("Region: ", current_region)
+    bucket_name = create_bucket_name(bucket_prefix)
+    bucket_response = s3_connection.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={
+            'LocationConstraint': current_region})
+    print(bucket_name, bucket_response)
+    return bucket_name, bucket_response
 
 
 # Load env file for AWS Cli authentication
@@ -18,26 +31,31 @@ load_dotenv()
 aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 aws_session_token = os.getenv("AWS_SESSION_TOKEN")  # Optional
-curr_region = "eu-central-1"
+region = "eu-central-1"
+
+
+
 # Initialize Boto3 session
-session = boto3.Session(
-    aws_access_key_id=aws_access_key,
-    aws_secret_access_key=aws_secret_key,
-    # aws_session_token=aws_session_token,  # Optional
-    # region_name='curr_region'
-)
-s3_client = boto3.client('s3')
-s3_resource = boto3.resource('s3', region_name=curr_region)
+# session = boto3.Session(
+#      aws_access_key_id=aws_access_key,
+#      aws_secret_access_key=aws_secret_key,
+#      # aws_session_token=aws_session_token,  # Optional
+#      region_name=region
+#  )
+# current_region = session.region_name
+# print(session.region_name)
+# s3_client = boto3.client('s3')
+s3_resource = boto3.resource('s3', region_name=region)
 bucket_prefix = "boto3-demo-"
-for bucket in s3_resource.buckets.filter(BucketRegion=curr_region, Prefix=bucket_prefix):
+for bucket in s3_resource.buckets.filter(BucketRegion=region, Prefix=bucket_prefix):
     print(bucket.name)
 
-
-bucket_name = create_bucket_name(bucket_prefix)
-s3_resource.create_bucket(Bucket=bucket_name,
-                          CreateBucketConfiguration={
-                              'LocationConstraint': curr_region})
-
-for bucket in s3_resource.buckets.filter(BucketRegion=curr_region, Prefix=bucket_prefix):
+for bucket in s3_resource.buckets.filter(BucketRegion=region, Prefix=bucket_prefix):
     print("--------------Buckets after the execution------------------------")
     print(bucket.name)
+
+first_bucket_name, first_response = create_bucket(
+    bucket_prefix=bucket_prefix,
+    s3_connection=s3_resource)
+
+print(first_response)
